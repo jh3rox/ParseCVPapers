d8e3795978297facb001945737e97a0cf5c5d283
jh3rox <jh3rox@gmail.com>
2013-06-09 21:16:29 +0900

    Fixing a bug in output function

--- ParseCVPapers.pl
+++ ParseCVPapers.pl
@@ -9,8 +9,8 @@ use utf8;
 use HTML::TreeBuilder;
 
 # Input(HTML) & Output(TXT)
-my $inHtml = "./cvpapers_cvpr2013.html";
-my $outTxt = "./cvpapers_cvpr2013.txt";
+my $inHtml = "./cvpr2013.html";
+my $outTxt = "./cvpr2013.txt";
 
 # Parsing HTML 
 my $tree = new HTML::TreeBuilder;
@@ -20,7 +20,6 @@ $tree->eof();
 # Finding Titles(<dt>) & Authors(<dd>) from HTML
 my @dtItems = $tree->look_down('id', 'content')->find('dt');
 my @ddItems = $tree->look_down('id', 'content')->find('dd');
-$tree = $tree->delete;
 my $nNumDt = $#dtItems;
 my $nNumDd = $#ddItems;
 if( $nNumDt != $nNumDd ){
@@ -32,10 +31,10 @@ if( $nNumDt != $nNumDd ){
 open( OUT, ">$outTxt" ) or die "$!";
 printf( OUT "No \t Title \t Author \n" );
 for( my $i=0; $i<=$nNumDt; $i++ ){
-    printf OUT "%d \t \"%s\" \t \"%s\" \n",
-            $i+1, $dtItems[$i]->as_text, $ddItems[$i]->as_text;
+    printf OUT "%d \t \"%s\" \t \"%s\" \n", $i+1, $dtItems[$i]->as_text, $ddItems[$i]->as_text;
 }
 close (OUT);
+$tree = $tree->delete;
 
 print "[Input]", $inHtml ,"\n";
 print "[Output]", $outTxt ,"\n";
